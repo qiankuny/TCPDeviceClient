2a216be9c658684e504af2c08133eb9936ff6a8c
qiankuny <qiankuny@163.com>
2017-07-28 15:17:37 +0800

    第二次提交

--- mod_config.py
+++ mod_config.py
@@ -9,4 +9,7 @@ def getConfig(section, key):
     config = ConfigParser.ConfigParser()
     path = os.path.split(os.path.realpath(__file__))[0] + '/config.conf'
     config.read(path)
-    return config.get(section, key)
+    try:
+        return config.get(section, key)
+    except:
+        return False
